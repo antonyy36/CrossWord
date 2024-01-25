c64a25cc94ef5e6e3546961ff33a13644b704431
ANTON <filatov21082007@gmail.com>
2024-01-25 22:26:58 +0300

    commit bro2

--- CrossWord.py
+++ CrossWord.py
@@ -1,10 +1,9 @@
-import sys
 import time
 
 import pygame
 pygame.init()
 
-W, H = 1500, 600
+W, H = 1200, 600
 FPS = 30
 clock_ = pygame.time.Clock()
 
@@ -12,13 +11,15 @@ pygame.display.set_caption('Кроссворд по теме "Информаци
 
 
 scr = pygame.display.set_mode((W, H))
-s = pygame.Surface((W - 785, 540))
-s.set_alpha(90)
-s.fill((255, 255, 255))
-font_schet = pygame.font.Font('Fonts/Intro.ttf', 22)
-font_schet1 = pygame.font.Font('Fonts/Intro.ttf', 20)
-font_text_cross = pygame.font.Font('Fonts/BoredersDivide.ttf', 20)
-image_fon = pygame.image.load('images/FON_FOR_CROSSWORD.png').convert_alpha()
+s = pygame.Surface((610, 540))
+s.set_alpha(120)
+s.fill((220, 220, 255))
+font_schet = pygame.font.Font('Fonts/Intro.ttf', 19)
+font_schet1 = pygame.font.Font('Fonts/Intro.ttf', 18)
+font_text_cross = pygame.font.Font('Fonts/saars.ttf', 17)
+font_text_cross_mesg = pygame.font.Font('Fonts/saars.ttf', 24)
+font_text_cross_nums = pygame.font.Font('Fonts/BoredersDivide.ttf', 18)
+image_fon = pygame.image.load('images/fon2.png').convert_alpha()
 
 text_for_crossword = [['1. ... объектов информатизации - комплекс организационных и технических мероприятий,',
                        '     в результате которых подтверждается соответствие системы защиты информации',
@@ -48,7 +49,7 @@ class Word:
         self.count = count_letters
         self.word = word
         self.curr_word = ''
-        self.cell_size = 25
+        self.cell_size = 20
         self.x = x
         self.y = y
         self.fl = False
@@ -64,15 +65,15 @@ class Word:
         if self.is_current:
             self.color_num = 'red'
         else:
-            self.color_num = 'black'
+            self.color_num = '#03f0fc'
         for i in range(1, len(self.word) + 1):
             for j in range(len(self.spisok_letters)):
                 if j == len(self.curr_word) and self.is_current:
                     self.spisok_letters[j].draw('red', 2)
                 else:
-                    self.spisok_letters[j].draw('black', 1)
+                    self.spisok_letters[j].draw('#03f0fc', 1)
                 txt_chsl = font_schet1.render(str(self.count), True, self.color_num)
-                scr.blit(txt_chsl, (self.x + 10, self.y))
+                scr.blit(txt_chsl, (self.x + 5, self.y))
 
     def add_letter(self, key):
         if len(self.curr_word + key) <= len(self.word):
@@ -88,14 +89,18 @@ class Word:
         if not self.spisok_letters[len(self.curr_word) - 1].is_checkd:
             self.spisok_letters[len(self.curr_word) - 1].letter = ''
             self.curr_word = self.curr_word[:-1]
-        else:
+        elif not self.spisok_letters[len(self.curr_word) - 2].is_checkd:
             self.spisok_letters[len(self.curr_word) - 2].letter = ''
             self.curr_word = self.curr_word[:-2]
 
     def proverka(self):
+        global txt_message
         if self.curr_word == self.word:
             for i in self.spisok_letters:
                 i.is_checkd = True
+            txt_message = 'Правильно!'
+        else:
+            txt_message = 'Неверно!'
 
     def proverka_full(self):
         for i in self.spisok_letters:
@@ -109,7 +114,7 @@ class Word_vert:
         self.count = count_letters
         self.word = word
         self.curr_word = ''
-        self.cell_size = 25
+        self.cell_size = 20
         self.x = x
         self.y = y
         self.is_current = False
@@ -131,13 +136,13 @@ class Word_vert:
         if self.is_current:
             self.color_num = 'red'
         else:
-            self.color_num = 'black'
+            self.color_num = '#03f0fc'
         for i in range(1, len(self.word) + 1):
             for j in range(len(self.spisok_letters)):
                 if j == len(self.curr_word) and self.is_current:
                     self.spisok_letters[j].draw('red', 2)
                 else:
-                    self.spisok_letters[j].draw('black', 1)
+                    self.spisok_letters[j].draw('#03f0fc', 1)
                 txt_chsl = font_schet1.render(str(self.count), True, self.color_num)
                 scr.blit(txt_chsl, (self.x, self.y - 20))
 
@@ -155,14 +160,18 @@ class Word_vert:
         if not self.spisok_letters[len(self.curr_word) - 1].is_checkd:
             self.spisok_letters[len(self.curr_word) - 1].letter = ''
             self.curr_word = self.curr_word[:-1]
-        else:
+        elif not self.spisok_letters[len(self.curr_word) - 2].is_checkd:
             self.spisok_letters[len(self.curr_word) - 2].letter = ''
             self.curr_word = self.curr_word[:-2]
 
     def proverka(self):
+        global txt_message
         if self.curr_word == self.word:
             for i in self.spisok_letters:
                 i.is_checkd = True
+            txt_message = 'Правильно!'
+        else:
+            txt_message = 'Неверно!'
 
     def proverka_full(self):
         for i in self.spisok_letters:
@@ -176,27 +185,27 @@ class Letter:
         self.x = x
         self.y = y
         self.is_checkd = False
-        self.cell_size = 25
+        self.cell_size = 20
         self.letter = ''
-        self.color = 'black'
+        self.color = '#0022ff'
 
     def draw(self, color_blocks, w):
         pygame.draw.rect(scr, color_blocks, (self.x, self.y, self.cell_size, self.cell_size), w)
         if self.letter:
-            self.color = 'green' if self.is_checkd else 'black'
+            self.color = '#00ff62' if self.is_checkd else '#fcfffe'
             txt_surf = font_schet.render(self.letter, True, self.color)
             # pygame.draw.rect(scr, 'lightblue', (self.x + 2, self.y + 2, 21, 21))
-            x, y = self.x + (12.5 - txt_surf.get_width() // 2), \
-                   self.y + (12.5 - txt_surf.get_height() // 2)
+            x, y = self.x + (10 - txt_surf.get_width() // 2), \
+                   self.y + (10 - txt_surf.get_height() // 2)
             scr.blit(txt_surf, (x, y))
 
 
 spisok_slov = []
 spisok_all_letters = []
 spis_goriz = [(1, 'аттестация'), (5, "безопасность"), (3, "секретности"), (8, "информация")]
-spis_coor_goriz = [(475, 250), (125, 125), (300, 175), (0, 175)]
+spis_coor_goriz = [(380, 300), (100, 200), (240, 240), (0, 240)]
 spis_vertical = [(7, 'система'), (6, "носитель"), (4, "доменное"), (2, "обладатель")]
-spis_coor_vert = [(175, 25), (225, 100), (350, 100), (500, 175)]
+spis_coor_vert = [(140, 120), (180, 180), (280, 180), (400, 240)]
 for i in range(len(spis_goriz)):
     spisok_slov.append(Word(spis_goriz[i][0], spis_goriz[i][1],
                             spis_coor_goriz[i][0], spis_coor_goriz[i][1]))
@@ -212,6 +221,7 @@ ind_del = spisok_slov.index(first_word)
 spisok_slov.insert(len(spisok_slov), first_word)
 spisok_slov.pop(ind_del)
 first_word.is_current = True
+txt_message = ''
 while f:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -243,21 +253,30 @@ while f:
                                    'р', 'о', 'л', 'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю']:
                 first_word.add_letter(event.unicode)
     scr.fill('lightblue')
-    scr.blit(image_fon, (0, 0))
-    scr.blit(s, (760, 20))
-    co = 0
+    scr.blit(image_fon, (0, -37))
+    scr.blit(s, (625, 20))
+    co = 8
     for i in range(len(text_for_crossword)):
         y = co + 20
         for j in range(len(text_for_crossword[i])):
             co += 20
-            text_cross_output = font_text_cross.render(text_for_crossword[i][j], True, 'black')
-            scr.blit(text_cross_output, (770, y + j * 20))
+            text_cross_output_num = font_text_cross_nums.render(text_for_crossword[i][j][0], True, '#03f0fc')
+            scr.blit(text_cross_output_num, (630, y + j * 20 - 4))
+            text_cross_output = font_text_cross.render(text_for_crossword[i][j][1:], True, '#03f0fc')
+            scr.blit(text_cross_output, (640, y + j * 20))
             if j + 1 == len(text_for_crossword[i]):
                 co += 15
     for i in spisok_slov:
         i.draw_rects()
     if all([i.proverka_full() for i in spisok_slov]):
-        text_win = font_text_cross.render('Вы успешно выполнили кроссворд!', True, 'black')
+        text_win = font_text_cross_mesg.render('Вы успешно выполнили кроссворд!', True, '#03f0fc')
+        scr.blit(text_win, (100, 550))
+        pygame.display.update()
+        time.sleep(4)
+        pygame.quit()
+        break
+    else:
+        text_win = font_text_cross_mesg.render(txt_message, True, '#03f0fc')
         scr.blit(text_win, (100, 550))
     pygame.display.update()
     clock_.tick(FPS)
